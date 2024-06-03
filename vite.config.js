d30756d0787ce6759bf06bddadb34f381edd2b69
sallescosta <newcapital.in@gmail.com>
2024-06-03 12:50:48 +0200

    fix: triing to fix vite deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 })
