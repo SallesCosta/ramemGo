7965745ab26550d5b09839f887dd4709b3a2a4dc
sallescosta <newcapital.in@gmail.com>
2024-06-12 13:09:05 +0200

    test: oi elemento

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import "./main-style.scss"
 
-import { heroElement } from "./components/hero.ts"
-import { contentElement } from "./components/content.ts"
+// import { heroElement } from "./components/hero.ts"
+// import { contentElement } from "./components/content.ts"
+
+const oiElement = document.createElement("div")
+oiElement.innerText = "Oi"
 
 const mainElement = document.createElement("main")
-mainElement?.appendChild(heroElement)
-mainElement?.appendChild(contentElement)
+// mainElement?.appendChild(heroElement)
+// mainElement?.appendChild(contentElement)
+mainElement?.appendChild(oiElement)
 
 const App = document.querySelector<HTMLDivElement>('[data-js="app"]')
 App?.appendChild(mainElement)
