0e353c0fa1b1c685c624df1025b6a1fd8d1d8c9a
sallescosta <newcapital.in@gmail.com>
2024-06-12 13:58:49 +0200

    add build

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import "./main-style.scss"
 
-// import { heroElement } from "./components/hero.ts"
-// import { contentElement } from "./components/content.ts"
-
-const oiElement = document.createElement("div")
-oiElement.innerText = "Oi"
+import { heroElement } from "./components/hero.ts"
+import { contentElement } from "./components/content.ts"
 
 const mainElement = document.createElement("main")
-// mainElement?.appendChild(heroElement)
-// mainElement?.appendChild(contentElement)
-mainElement?.appendChild(oiElement)
+mainElement?.appendChild(heroElement)
+mainElement?.appendChild(contentElement)
 
 const App = document.querySelector<HTMLDivElement>('[data-js="app"]')
 App?.appendChild(mainElement)
